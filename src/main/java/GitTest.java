2f49a9b2f2f8c365913476dad12187b96a9f458f
LzhA <780574aaa@163.com>
2021-06-16 20:04:21 +0800

    push commit

--- src/main/java/GitTest.java
+++ src/main/java/GitTest.java
@@ -7,5 +7,6 @@ public class GitTest {
         System.out.println("hello git5");
         System.out.println("hello master");
         System.out.println("hello newC");
+        System.out.println("推送代码");
     }
 }
